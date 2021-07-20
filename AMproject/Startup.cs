089e22ab319f6acd7b0634c1c937a5b2d4d3d4d1
ramyamukund <35280536+ramyamukund@users.noreply.github.com>
2021-07-20 08:15:37 +0100

    changes to nav and starte page

--- AMproject/Startup.cs
+++ AMproject/Startup.cs
@@ -54,7 +54,7 @@ namespace AMproject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=User}/{action=Index}/{id?}");
             });
         }
     }
